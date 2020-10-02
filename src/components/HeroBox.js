158f6d429a26ad745dc06e219b939ee3043d7f5e
John Bowser <john.bow7720@gmail.com>
2020-10-01 18:38:19 -0600

    added utahjs logo, adjusted fonts

--- src/components/HeroBox.js
+++ src/components/HeroBox.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
+import heroBoxStyles from '../styles/hero-box.module.css'
+import logo from '../images/utahjs-logo.png'
+
 const HeroBox = () => {
   return (
-    <div className='hero-box'>
-      <h2>UtahJS</h2>
-      <div className='hero-subtext'>
-        JavaScript Engineers of Utah
-      </div>
-      <div className='hero-subtext'>
-        Let's learn together.
+    <div className={heroBoxStyles.hero_box}>
+      <div className={heroBoxStyles.hero_box_inner}>
+        <img className={heroBoxStyles.logo} src={logo} alt='UtahJS Logo' />
+        <h2>UtahJS</h2>
+        <div className={heroBoxStyles.hero_subtext}>
+          JavaScript Engineers of Utah
+        </div>
+        <div className={heroBoxStyles.hero_subtext}>
+          Let's learn together.
+        </div>
       </div>
     </div>
   )
