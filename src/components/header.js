48c4534c42cb3ec190135d14f24f4c670dc0c91c
Danny Oka <dannyoka@gmail.com>
2020-10-02 21:08:23 -0600

    added Header component and updated header CSS in layout.css

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,68 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <React.Fragment>
+    <header className="center-it">
+      <div className="content center-content">
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#F7DD56`,
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          <img
+            className="header-logo"
+            src="https://conf.utahjs.com/img/header-logo.png"
+            alt="UTAHJS"
+            style={{
+              float: "left",
+              marginRight: "20px",
+              verticalAlign: "middle",
+              border: "0",
+              height: "65px",
+            }}
+          />
         </Link>
-      </h1>
-    </div>
-  </header>
+        <div
+          className="title"
+          style={{
+            float: "left",
+            fontSize: "25px",
+            verticalAlign: "middle",
+            color: "#f7dd56",
+            fontWeight: "bold",
+            fontFamily: "Domine, serif",
+            marginTop: "22px",
+            marginBottom: "11px",
+            marginLeft: "10px",
+          }}
+        >
+          2020 UtahJS Conference - Online Series
+        </div>
+      </div>
+    </header>
+    <header>
+      <nav className="center-it">
+        <div className="center-content">
+          <Link to="/">HOME</Link>
+          <Link to="https://vi.to/hubs/utahjs-conference-series">TICKETS</Link>
+          <Link to="/code-of-conduct">CODE OF CONDUCT</Link>
+          <Link
+            to="/https://teespring.com/utahjs-2020-online-series"
+            target="_blank"
+          >
+            MERCH
+          </Link>
+          <Link to="/sponsor">SPONSOR US</Link>
+          <Link to="/speakers-2019">PAST SPEAKERS</Link>
+          <Link to="http://eepurl.com/hcwmDf">MAILING LIST</Link>
+          <Link to="http://utahjs.com">UTAHJS.COM</Link>
+          <Link to="http://slack.utah.js">SLACK</Link>
+        </div>
+      </nav>
+    </header>
+  </React.Fragment>
 )
 
 Header.propTypes = {
