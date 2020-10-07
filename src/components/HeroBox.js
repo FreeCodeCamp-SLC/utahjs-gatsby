0b859b3dc02c08f2a1a3ef3a81d89ed6a719a583
John Bowser <john.bow7720@gmail.com>
2020-10-06 18:44:58 -0600

    switched divs to ps, renamed svg file

--- src/components/HeroBox.js
+++ src/components/HeroBox.js
@@ -9,12 +9,10 @@ const HeroBox = () => {
       <div className={heroBoxStyles.hero_box_inner}>
         <img className={heroBoxStyles.logo} src={logo} alt='UtahJS Logo' />
         <h2>UtahJS</h2>
-        <div className={heroBoxStyles.hero_subtext}>
-          JavaScript Engineers of Utah
-        </div>
-        <div className={heroBoxStyles.hero_subtext}>
-          Let's learn together.
-        </div>
+        
+        <p className={heroBoxStyles.hero_subtext}>JavaScript Engineers of Utah</p>
+        <p className={heroBoxStyles.hero_subtext}>Let's learn together.</p>
+        
         <a className={`${heroBoxStyles.btn} ${heroBoxStyles.yellow}`} href='https://conf.utahjs.com'>Conference</a>
         <a className={`${heroBoxStyles.btn} ${heroBoxStyles.green}`} href='https://slack.utahjs.com'>Join Slack</a>
       </div>
