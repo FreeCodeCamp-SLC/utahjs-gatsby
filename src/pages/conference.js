65d5a04e81d424e6fa3da465311748eaaa8f045c
AlexanderPuhl <alexpuhldeveloper@gmail.com>
2020-10-09 15:27:54 -0600

    Added the layout component to the conference page

--- src/pages/conference.js
+++ src/pages/conference.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React from "react"
+
+import Layout from "../components/layout"
 
 const conferencePage = () => (
-	<h1>Conference Page</h1>
+  <Layout>
+    <h1>Conference Page</h1>
+  </Layout>
 )
 
 export default conferencePage
