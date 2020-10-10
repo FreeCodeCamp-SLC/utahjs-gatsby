c4175c03cd01982c47034b9133eca2e94e0cec0e
zorro1rr <zorro1rr@gmail.com>
2020-10-10 15:05:00 -0600

    added .vscode to .gitignore & edited markup/css on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,13 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <section className="about">
-      UtahJS is a 501(c)(3) organization and contributions are tax deductible.
+    <footer className="about">
+      <p>UtahJS is a 501(c)(3) organization and contributions are tax deductible.
       We are run by a passionate group of volunteers. To help out or make a
       donation or sponsor this site or an event please contact Ken Snyder at{" "}
       <a href="mailto:ken@utahjs.com">ken@utahjs.com</a>.
-    </section>
+      </p>
+    </footer>
   )
 }
 
