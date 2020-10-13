994f3d4cab38f26c9f4265297570ed31ecb82114
Mikel North <mikel.north@chghealthcare.com>
2020-10-13 16:49:17 -0600

    update eslint for sanity file

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/no-unresolved */
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from 'part:@sanity/base/schema-creator';
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,5 +13,5 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-  ])
-})
+  ]),
+});
