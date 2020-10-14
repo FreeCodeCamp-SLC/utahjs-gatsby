60512b59bd4dc85d0926467ab3e3c5fe36ee1707
Mikel North <mikel.north@gmail.com>
2020-10-13 18:54:44 -0600

    setup sanity (#27)
    * setup sanity
    
    * update package json
    
    * remove rc files
    
    * eslint fix
    
    * update eslint for sanity file
    
    * add vscode files to git
    
    Co-authored-by: Mikel North <mikel.north@chghealthcare.com>

---
+++ gatsby/src/components/layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+
+import Header from './header';
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <main>{children}</main>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
