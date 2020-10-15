1efea04909cba4975441fe2b5232218434742763
zorro1rr <zorro1rr@gmail.com>
2020-10-15 16:36:41 -0600

    merged from master

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -1,17 +1,14 @@
-import React from "react"
+import React from 'react';
 
-const Footer = () => {
-  return (
-    <footer className="about">
-      <p>UtahJS is a 501(c)(3) organization and contributions are tax deductible.
+const Footer = () => (
+  <footer className="about">
+    <p>
+      UtahJS is a 501(c)(3) organization and contributions are tax deductible.
       We are run by a passionate group of volunteers. To help out or make a
-      donation or sponsor this site or an event please contact Ken Snyder at{" "}
+      donation or sponsor this site or an event please contact Ken Snyder at{' '}
       <a href="mailto:ken@utahjs.com">ken@utahjs.com</a>.
-      </p>
+    </p>
+  </footer>
+);
 
-      
-    </footer>
-  )
-}
-
-export default Footer
+export default Footer;
