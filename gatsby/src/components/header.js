e4a33501458361a2f9eb06af92f4efe7ba96e8ac
Mikel North <mikel.north@chghealthcare.com>
2020-10-20 19:07:32 -0600

    change

--- gatsby/src/components/header.js
+++ gatsby/src/components/header.js
@@ -19,7 +19,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
