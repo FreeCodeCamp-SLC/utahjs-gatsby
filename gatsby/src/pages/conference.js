3d44741d7016846e7070c04f7771810eb0cfec5c
Alex Puhl <alexpuhldeveloper@gmail.com>
2020-10-20 18:48:48 -0600

    Added the conference info section to the conference page (#32)
    * Added the conference info section to the conference page
    
    * Updated a tags to be Link tags
    
    * updated link text

--- gatsby/src/pages/conference.js
+++ gatsby/src/pages/conference.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+const CenterContentStyles = styled.div`
+  max-width: 1240px;
+  padding: 0 20px;
+  margin: 0 auto;
+  text-align: left;
+`;
+
 const conferencePage = () => (
   <Layout>
     <SEO title="Conference" />
-    <h1>Conference Page</h1>
+    <CenterContentStyles>
+      <div>Hero section placeholder</div>
+      <section className="conference_info">
+        <h1>9th Annual UtahJS Conference - Online Series</h1>
+        <p>
+          Due to COVID-19, the 2020 UtahJS Conference will be an online series.
+        </p>
+        <p>
+          Rather than an all-day conference, we will be hosting four lunch hour
+          presentations.
+        </p>
+        <p>
+          As always, we price tickets to cover our costs. This year, $20
+          includes admission to all 4 presentations! Thanks for supporting the
+          UtahJS Community.
+        </p>
+        <p>
+          We are using the new and very slick Vi.to platform to stream the
+          presentation and host a Q&amp;A session afterward. You can chat with
+          speakers and attendees and ask questions before, during and after the
+          presentations.
+        </p>
+        <p>
+          We are super excited to hear these speakers and hope you will join us!
+        </p>
+      </section>
+    </CenterContentStyles>
   </Layout>
 );
 
