50bfce57e541e0100669c41aa54479c92b2e4141
John Bowser <john.bow7720@gmail.com>
2020-10-20 19:27:19 -0600

    cleaned up prettier problems

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Hero from '../components/Hero'
+import Hero from '../components/Hero';
 import { HomeStyles, CenterContent } from '../styles/indexStyles';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
