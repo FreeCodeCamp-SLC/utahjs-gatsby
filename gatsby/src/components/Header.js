330d9787fbcbe781579025b9925a3a60c43f08a2
zorro1rr <zorro1rr@gmail.com>
2020-12-06 10:32:39 -0700

    tweaked styling on header component

--- gatsby/src/components/Header.js
+++ gatsby/src/components/Header.js
@@ -4,24 +4,24 @@ import styled from 'styled-components';
 import Img from 'gatsby-image';
 
 const Wrapper = styled.div`
-  background: #333333;
-`;
-
-const Head = styled.div`
-  max-width: 1240px;
-  margin: 0 auto;
-  text-align: left;
-  display: flex;
-  align-items: center;
-  height: 100px;
-  background: #333333;
-  div {
+  background: #2b2b2b;
+  header {
+    max-width: 1240px;
+    margin: 0 auto;
+    text-align: left;
+    display: flex;
+    align-items: center;
+    height: 100px;
+    background: #2b2b2b;
+  }
+  .imageWrap {
     margin: 10px 18px 7px 10px;
   }
   h1 {
     color: #f7dd56;
     font-weight: bold;
     font-family: Domine, serif;
+    font-size: 25px;
     margin-top: 22px;
     margin-bottom: 11px;
     margin-left: 10px;
@@ -42,14 +42,14 @@ function Header() {
   `);
   return (
     <Wrapper>
-      <Head>
+      <header>
         <Link to="/">
-          <div>
+          <div className="imageWrap">
             <Img fixed={data.utahjs.childImageSharp.fixed} alt="Utah JS Logo" />
           </div>
         </Link>
         <h1>{new Date().getFullYear()} UtahJS Conference - Online Series</h1>
-      </Head>
+      </header>
     </Wrapper>
   );
 }
---
+++ gatsby/src/pages/past-speakers.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled from 'styled-components';
+import Speaker from '../components/Speaker';
+import { speakers } from '../data/speakers';
+import Layout from '../components/Layout';
+
+const PageStyles = styled.div`
+  font-family: Domine, serif;
+  max-width: 1240px;
+  margin: 0 auto;
+  color: #333;
+  padding: 16px;
+  height: 100vh;
+  .bold {
+    font-size: 26px;
+    font-weight: 600;
+  }
+  .italic {
+    font-style: italic;
+    font-family: Gothic A1;
+  }
+  #wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  #speakers {
+    color: #ff9000;
+    font-family: Gothic A1;
+  }
+`;
+
+const AgendaManage = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 11px;
+  font-family: sans-serif;
+  text-decoration: none;
+  margin-bottom: 17px;
+  .strong {
+    color: rgba(17, 17, 17, 0.5);
+    font-weight: 600;
+    transition: all 0.15s ease-in;
+  }
+  a {
+    color: rgba(17, 17, 17, 0.5);
+  }
+  a:hover {
+    #teal {
+      color: #1ab394;
+    }
+  }
+`;
+
+export default function SpeakersPage() {
+  return (
+    <Layout>
+      <nav>bunch of alinks</nav>
+      <PageStyles>
+        <div id="wrapper">
+          <h2 className="bold">2019 Speakers</h2>
+          <p className="italic">in alphabetical order</p>
+          <p id="speakers">View 2018 Speakers</p>
+          {speakers.map((speaker) => (
+            <Speaker speaker={speaker} />
+          ))}
+        </div>
+        <AgendaManage>
+          <a className="manage" href="https://sessionize.com/">
+            <span className="strong">Agenda Management</span> powered by{' '}
+            <span className="strong" id="teal">
+              Sessionize.com
+            </span>
+          </a>
+        </AgendaManage>
+      </PageStyles>
+    </Layout>
+  );
+}
