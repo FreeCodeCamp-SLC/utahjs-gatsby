56d7f05583d38129fe5275d4fc90cce7653bf21f
John Bowser <john.bow7720@gmail.com>
2020-12-08 16:33:28 -0700

    image is showing up, need to address some resizing bugs

--- gatsby/src/components/Hero.js
+++ gatsby/src/components/Hero.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import styled from 'styled-components';
 import HeroBox from './HeroBox';
 import heroImg from '../images/Circuit_Board.svg';
@@ -8,14 +10,42 @@ const HeroStyles = styled.div`
   max-width: 1240px;
   min-height: 320px;
   height: 35vh;
-  background: url(${heroImg});
   margin: 0 auto;
+
 `;
 
-const Hero = () => (
-  <HeroStyles>
-    <HeroBox />
-  </HeroStyles>
-);
+// const HeroImg = styled(Img)`
+//   position: "absolute",
+//   left: 0,
+//   top: 0,
+//   width: "100%",
+//   height: "100%",
+//   z-index: -1
+// `;
+
+const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "arch.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <HeroStyles>
+      <Img
+        className="heroImg"
+        fluid={data.file.childImageSharp.fluid}
+        alt="Arch"
+      />
+      <HeroBox />
+    </HeroStyles>
+  );
+};
 
 export default Hero;
