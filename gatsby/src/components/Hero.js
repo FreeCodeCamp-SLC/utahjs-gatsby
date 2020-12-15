534c816d92fc865e61b3767e38d20a4952ceac54
John Bowser <66637570+jgbowser@users.noreply.github.com>
2020-12-15 14:25:55 -0700

    Landing page hero img (#73)
    * set up hero and hero box components, removed boilerplate code
    
    * added basic styling, min height to hmtl, body, index
    
    * added utahjs logo, adjusted fonts
    
    * completed hero image/content
    
    * basic media query for larger screens
    
    * switched divs to ps, renamed svg file
    
    * fixed styling affected by earlier merge
    
    * move styles to styled components
    
    * replaced anchor tags with react-bootstrap Buttons
    
    * fixed issues with merge
    
    * cleaned up prettier problems
    
    * image is showing up, need to address some resizing bugs
    
    * experimenting with image resize
    
    * arches image is now present, will require some work on styling and responsiveness

--- gatsby/src/components/Hero.js
+++ gatsby/src/components/Hero.js
@@ -1,21 +1,55 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import styled from 'styled-components';
 import HeroBox from './HeroBox';
 import heroImg from '../images/Circuit_Board.svg';
+
+// In order to use gatsby-image and its benefits the background image is
+// queried with GraphQL then fed to an Img component. The component is then
+// styled to act like a background image with an absolute position and 100% coverage
 
 const HeroStyles = styled.div`
   position: relative;
   max-width: 1240px;
   min-height: 320px;
   height: 35vh;
-  background: url(${heroImg});
   margin: 0 auto;
+  z-index: 1;
+
+  heroImg {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -10;
+  }
 `;
 
-const Hero = () => (
-  <HeroStyles>
-    <HeroBox />
-  </HeroStyles>
-);
+const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "arch.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <HeroStyles>
+      <Img
+        className="heroImg"
+        fluid={data.file.childImageSharp.fluid}
+        alt="Arch"
+      />
+      <HeroBox />
+    </HeroStyles>
+  );
+};
 
 export default Hero;
