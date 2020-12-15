656b6c1ecbd759b4fe8d97384cb3ad6061ac7b1d
Alex Puhl <alexpuhldeveloper@gmail.com>
2020-12-15 16:02:05 -0700

    Cleaned up the layout component

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import 'normalize.css';
-import { useStaticQuery, graphql } from 'gatsby';
 import GlobalStyles from '../styles/main';
 import Header from './Header';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  return (
-    <>
-      <GlobalStyles />
-      <Header />
-      <main>{children}</main>
-    </>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <GlobalStyles />
+    <Header />
+    <main>{children}</main>
+  </>
+);
 
 export default Layout;
