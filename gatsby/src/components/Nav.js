7dee92455830304d126aba3b3bcb493b307772b5
Alex Puhl <alexpuhldeveloper@gmail.com>
2021-02-15 09:55:05 -0700

    Made the navbar dynamic in case they want to delete a nav link

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -82,129 +82,50 @@ const StyledLink = styled(GatsbyLink)`
   }
 `;
 const Nav = ({ toggle }) => {
-  const { sanityNavigation } = useStaticQuery(graphql`
+  const { allSanityNavigation } = useStaticQuery(graphql`
     query nav {
-      sanityNavigation {
-        link_1_text
-        link_1_url
-        link_1_target
-        link_2_text
-        link_2_url
-        link_2_target
-        link_3_text
-        link_3_url
-        link_3_target
-        link_4_text
-        link_4_url
-        link_4_target
-        link_5_text
-        link_5_url
-        link_5_target
-        link_6_text
-        link_6_url
-        link_6_target
-        link_7_text
-        link_7_url
-        link_7_target
-        link_8_text
-        link_8_url
-        link_8_target
-        link_9_text
-        link_9_url
-        link_9_target
+      allSanityNavigation(sort: { order: ASC, fields: order }) {
+        nodes {
+          link_text
+          internal_link
+          external_link
+          new_tab
+          no_follow
+          order
+        }
       }
     }
   `);
+  const navArray = allSanityNavigation.nodes;
+  console.log(navArray);
 
   return (
     <NavStyles>
       <div className={!toggle ? 'navDiv Close' : 'navDiv Open'}>
         <ul>
-          <li>
-            <StyledLink
-              to={sanityNavigation.link_1_url}
-              target={sanityNavigation.link_1_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_1_text}
-            </StyledLink>
-          </li>
-          <li>
-            <StyledLink
-              to={sanityNavigation.link_2_url}
-              target={sanityNavigation.link_2_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_2_text}
-            </StyledLink>
-          </li>
-          <li>
-            <a
-              className="linkDiv"
-              href={sanityNavigation.link_3_url}
-              target={sanityNavigation.link_3_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_3_text}
-            </a>
-          </li>
-          <li>
-            <StyledLink
-              to={sanityNavigation.link_4_url}
-              target={sanityNavigation.link_4_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_4_text}
-            </StyledLink>
-          </li>
-          <li>
-            <a
-              className="linkDiv"
-              href={sanityNavigation.link_5_url}
-              target={sanityNavigation.link_5_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_5_text}
-            </a>
-          </li>
-          <li>
-            <StyledLink
-              to={sanityNavigation.link_6_url}
-              target={sanityNavigation.link_6_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_6_text}
-            </StyledLink>
-          </li>
-          <li>
-            <StyledLink
-              to={sanityNavigation.link_7_url}
-              target={sanityNavigation.link_7_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_7_text}
-            </StyledLink>
-          </li>
-          <li>
-            <a
-              className="linkDiv"
-              href={sanityNavigation.link_8_url}
-              target={sanityNavigation.link_8_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_8_text}
-            </a>
-          </li>
-          <li>
-            <a
-              className="linkDiv"
-              href={sanityNavigation.link_9_url}
-              target={sanityNavigation.link_9_target}
-              rel="noreferrer"
-            >
-              {sanityNavigation.link_9_text}
-            </a>
-          </li>
+          {navArray.map((nav, id) =>
+            nav.internal_link !== null ? (
+              <li key={id}>
+                <StyledLink
+                  to={nav.internal_link}
+                  target={nav.new_tab === true ? '_blank' : ''}
+                  rel="noreferrer"
+                >
+                  {nav.link_text}
+                </StyledLink>
+              </li>
+            ) : (
+              <li key={id}>
+                <a
+                  href={nav.external_link}
+                  target={nav.new_tab === true ? '_blank' : ''}
+                  rel="noreferrer"
+                >
+                  {nav.link_text}
+                </a>
+              </li>
+            )
+          )}
         </ul>
       </div>
     </NavStyles>
